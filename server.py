00b1562e841a14f263bafc348a20e497e8fbddf5
Jesse van Elteren <jvanelteren@gmail.com>
2020-11-01 21:09:30 +0100

    added model to server

--- server.py
+++ server.py
@@ -1,3 +1,30 @@
+#%%
+import torch, torchvision, torch.nn as nn
+
+class AgeResnet(nn.Module):
+    def __init__(self, size='18', feat_extract=False):
+        super().__init__()
+        resnet = 'torchvision.models.resnet'+size+'(pretrained=True)'
+        resnet = eval(resnet)
+        modules=list(resnet.children())[:-1]
+        self.resnet =nn.Sequential(*modules)
+
+        if feat_extract:
+            # with feature extraction we only train the linear layer and keep the resnet parameters fixed 
+            for m in self.modules():
+                m.requires_grad_(False)
+
+        self.fc = nn.Linear(in_features=512, out_features=1, bias=True)
+        nn.init.kaiming_normal_(self.fc.weight)
+
+    def forward(self,x):
+        out = self.resnet(x)
+        x = torch.flatten(out, 1)
+        return self.fc(x)
+
+model = AgeResnet()
+model.load_state_dict('model37')
+#%%
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
