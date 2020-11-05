6bf3985d050190d23a52624084360cf476eb6e0d
Jesse van Elteren <jvanelteren@gmail.com>
2020-11-05 17:22:42 +0100

    working batch generator

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#%%
 from enum import Enum
 from typing import Optional, List
 from fastapi import FastAPI
@@ -31,9 +32,38 @@ logger.setLevel(logging.DEBUG)
 logger.debug("Debug test") # the log level needs to be set here and not in uvicorn!
 
 
-@app.get("/get_images/") #use post since server receives
+#%%
+
+# opening pickle file
+import pickle
+import random
+from itertools import cycle
+f = open("predictions.pickle","rb")
+df = pickle.load(f)
+logger.debug(len(df))
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        res = lst[i:i + n]
+        if len(res)==n:
+            yield res
+
+def gen_img_batch(n):
+    img_ids = list(range(len(df)))
+    random.shuffle(img_ids)
+    yield from cycle(chunks(img_ids,n))
+
+img_batch_generator  = gen_img_batch(n=3)
+ids = next(img_batch_generator)
+print(ids)
+df.iloc[ids]
+#%%"/get_images/") #use post since server receives
 async def return_images():
     logger.debug('get images') # this is the way to use the pydantic base model
+
+    ids = next(img_batch_generator)
+
     faces = list(range(10))
     faceids = list(range(20,30))
     computer = list(range(30,40))
@@ -51,12 +81,6 @@ async def submit_preds(ages:Ages):
     # TODO save ages to database
     return {'msg': 'ok'}
 
-import uvicorn
+#%%
 
-# def run():
-#     # LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
-#     uvicorn.run(app, host="127.0.0.1", port=8000, log_level='debug')
-
-
-# if __name__ == "__main__":
-#     run()
+# %%
