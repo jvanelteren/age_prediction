62f64af15489fb5e16ba8fdf378a54be9e7e48f5
Jesse van Elteren <jvanelteren@gmail.com>
2020-11-06 12:07:42 +0100

    made database with connection

---
+++ database.py
@@ -0,0 +1,75 @@
+import sqlite3
+from sqlite3 import Error
+
+def create_con(db_file):
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+        conn.commit()
+    except Error as e:
+        print(e)
+
+def create_pred(conn, prediction):
+    sql = """ INSERT INTO predictions(date, ip, image_path, prediction)
+              VALUES(DATE('now'),?,?,?) """
+    cur = conn.cursor()
+    cur.execute(sql,prediction)
+    conn.commit()
+    return cur.lastrowid
+
+def do_sql(conn,sql):
+    cur = conn.cursor()
+    cur.execute(sql)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+def count_predictions(conn):
+    cur = conn.cursor()
+    print(cur.execute("SELECT COUNT (*) FROM predictions;").fetchall()[0])
+
+def count_distinct_predictions(conn):
+    cur = conn.cursor()
+    print(cur.execute("SELECT COUNT(*) FROM (SELECT DISTINCT * FROM predictions);").fetchall()[0])
+
+def del_all_records(conn):
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS predictions")
+    cur.execute("DROP TABLE IF EXISTS games")
+    conn.commit()
+
+
+from sqlalchemy import create_engine
+
+def open_db(name):
+    sql_create_table_predictions = """ CREATE TABLE IF NOT EXISTS predictions(
+                                        prediction_id integer PRIMARY KEY,
+                                        date text NOT NULL,
+                                        ip text NOT NULL,
+                                        image_path text NOT NULL,
+                                        prediction integer NOT NULL)
+                                    """
+
+    conn = create_con(name)
+
+    if conn:
+        engine = create_engine('sqlite:///%s' % 'hi.db', echo=True)
+        # c = conn.cursor()
+        # c.execute(""" DROP TABLE IF EXISTS games""")
+        # create_table(conn, sql_create_table_games)
+        # c.execute(""" DROP TABLE IF EXISTS predictions""")
+        create_table(conn, sql_create_table_predictions)
+        
+    return conn
+
+def print_db(conn):
+    for i in conn.cursor().execute("SELECT * FROM predictions"):
+        print(i)
--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 #%%
 from enum import Enum
 from typing import Optional, List
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from fastapi.logger import logger
-from uvicorn.config import LOGGING_CONFIG
 import logging
+import pickle
+import random
+from itertools import cycle
 
-class Item(BaseModel):
-    user: str
-    pwd: Optional[str] = None
-    organization: str
-    required: Optional[str] = None
 
 class Ages(BaseModel):
     age: List[str] = []
     faceids: List[str] = []
 
-class User(BaseModel):
-    user: str
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-    
 app = FastAPI()
 
 logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.DEBUG)
-logger.debug("Debug test") # the log level needs to be set here and not in uvicorn!
+logger.setLevel(logging.DEBUG) # the log level needs to be set here and not in uvicorn!
 
 
 #%%
-
-# opening pickle file
-import pickle
-import random
-from itertools import cycle
-f = open("predictions.pickle","rb")
-df = pickle.load(f)
-logger.debug(len(df))
-
 def gen_img_ids():
     img_ids = list(range(len(df)))
     random.shuffle(img_ids)
@@ -50,15 +28,25 @@ def gen_img_ids():
 def next_batch(gen,n):
     return [next(gen) for _ in range(n)]
 
-img_ids_gen = gen_img_ids()
 
+# opening pickle file
+f = open("predictions.pickle","rb")
+df = pickle.load(f)
+
+logger.debug(f'number of items in dataset {len(df)}')
+img_batch_gen = gen_img_ids()
+
+import database as db
+conn = db.open_db('predictions.db')
+db.print_db(conn)
+logger.debug(f'{db.count_predictions(conn)} items in database')
 
 @app.get("/get_images/") 
 async def return_images():
     logger.debug('get images') # this is the way to use the pydantic base model
     n=10
 
-    batch_info_df = df.loc[next_batch(img_ids_gen,4)]
+    batch_info_df = df.loc[next_batch(img_batch_gen,4)]
 
     faces = list(range(10)) # this was a placeholder for images, but can be approached locally
     faceids = list(batch_info_df['path'])
@@ -71,12 +59,23 @@ async def return_images():
             }
 
 @app.post("/submit_preds/") #use post since server receives
-async def submit_preds(ages:Ages):
+async def submit_preds(ages:Ages,request: Request):
     logger.debug('submit preds') # this is the way to use the pydantic base model
+    logger.debug(request.client.host) # this is the way to use the pydantic base model
     logger.debug(ages)
     # TODO save ages to database
+    for i in range(len(ages.age)):
+        db.create_pred(conn, [request.client.host,ages.faceids[i],ages.age[i]])
+    db.print_db(conn)
+
+
     return {'msg': 'ok'}
 
 #%%
 
+
+# %%
+
+# %%
+# 
 # %%
