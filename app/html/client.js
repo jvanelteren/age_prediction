1495ccc40db103b08a53460aa471ac0b7c85ddbb
Jesse van Elteren <jvanelteren@gmail.com>
2020-11-09 20:34:10 +0000

    server working with image upload!

--- app/html/client.js
+++ app/html/client.js
@@ -9,14 +9,23 @@ var rows = document.getElementsByClassName('rows');
 
 function submit_start() {
     //  gets faces from server
-
     let xhr = new XMLHttpRequest();
+    xhr.timeout = 10000;
     // let url = ('GET', `${loc.protocol}//${loc.hostname}:${loc.port}/get_images`)
-    let url = 'http://127.0.0.1:8000/get_images/';
+    // let url = 'http://127.0.0.1:8001/backend/get_images/';
+    // let url = 'http://34.121.58.11/app/get_images/';
+    let url = '/backend/get_images/';
     
+    console.time('Execution Time');
     xhr.onload = function (e) {
+
         if (this.readyState === 4) {
+            console.timeEnd('Execution Time');
+            // alert('binnen!!!!');
+            // alert(e.target.responseText);
             let response = JSON.parse(e.target.responseText);
+            // alert(response['faces'][0]);
+            // alert(response['faceids'][0]);
             // el('demo').innerHTML = `Result = ${response['result']}`;
             for (var i = 0; i < faces.length; i++) {
                 // textToWrite = faces[i].value;
@@ -41,8 +50,9 @@ function submit_start() {
             window.value=response['faceids'];
     };
     }
-    xhr.onerror = function () { // only triggers if the request couldn't be made at all
+    xhr.onerror = function (e) { // only triggers if the request couldn't be made at all
         alert(`Network Error`);
+        alert(e);
     };
     // vscode make sure to debug with CORS enabled in launch.json "runtimeArgs": ["--disable-web-security"]
     xhr.open('GET', url, true);
@@ -60,9 +70,11 @@ function submit_start() {
     // }
 
 
-
+    xhr.timeout = 10000;
 
     xhr.send() // with optional [body]
+    xhr.timeout = 10000;
+
 }
 
 function submit_preds() {
@@ -70,7 +82,7 @@ function submit_preds() {
 
     
     let xhr = new XMLHttpRequest();
-    let url = 'http://127.0.0.1:8000/submit_preds/';
+    let url = '/backend/submit_preds/';
     
     xhr.onload = function (e) {
         if (this.readyState === 4) {
@@ -127,44 +139,42 @@ function submit_preds() {
 
 }
 
-function select_image() {
-    alert('select_image');
 
-
-} 
 
 function submit_image() {
-    alert('submit_image');
 
-    document.getElementById('img_predicted_age').innerHTML = 'predicted age';
+        //get the input and the file
+        var input = document.querySelector('input[type=file]'),
+        file = input.files[0];
+        let xhr = new XMLHttpRequest();
+
+        xhr.open("POST", "/backend/upload/", true);
+
     
-    let xhr = new XMLHttpRequest();
-    // let url = ('GET', `${loc.protocol}//${loc.hostname}:${loc.port}/get_images`)
-    let url = 'http://127.0.0.1:8000/app/get_images/';
+        //if the file isn't a image nothing happens.
+        //you are free to implement a fallback
+        if (!file || !file.type.match(/image.*/)) alert('hoooes');
     
-    xhr.onload = function (e) {
-        if (this.readyState === 4) {
-            alert(`Loaded`)
+        //Creates the FormData object and attach to a key name "file"
+    
+
+        var fd = new FormData();
+        fd.append("file", file);
+        
+        
+        
+        
+        xhr.onload = function(e) {
+            //The response of de upload
+            alert(xhr.responseText);
+            xhr.responseText;
+
+        }
             
-            document.getElementById(2).innerHTML = "blaaa";
-            let response = JSON.parse(e.target.responseText);
-            document.getElementById(2).innerHTML = "blaaa";
-                // el('demo').innerHTML = `Result = ${response['result']}`;
-            }
-    };
-
-    xhr.onerror = function () { // only triggers if the request couldn't be made at all
-        alert(`Network Error`);
-    };
-    // vscode make sure to debug with CORS enabled in launch.json "runtimeArgs": ["--disable-web-security"]
-    xhr.open('GET', url, true);
-    xhr.send([]) // with optional [body]
+            xhr.send(fd)
+            alert(document.getElementById('upload').value)
 } 
 
-
-
-
-document.getElementById('select_image').addEventListener("click", select_image)
 document.getElementById('submit_image').addEventListener("click", submit_image)
 document.getElementById('submit_preds').addEventListener("click", submit_preds)
 document.getElementById('submit_start').addEventListener("click", submit_start)
--- app/main.py
+++ app/main.py
@@ -4,18 +4,41 @@ from typing import Optional, List
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import logging
-import pickle
+import pickle5
 import random
 from itertools import cycle
+from fastapi.middleware.cors import CORSMiddleware # this is absolutely essential to get rid of these *** cors errors
+from fastapi import FastAPI, File, UploadFile, HTTPException, status
+import numpy as np
+from PIL import Image
+from io import BytesIO
+# you can specify allowed origins, or just allow everything with ["*"]
+origins = [
+    "http://34.121.58.11/",
+    "http://34.121.58.11",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-
+import os
+if os.getcwd()=='/ds/app':
+    os.chdir('/ds')
 class Ages(BaseModel):
     age: List[str] = []
     faceids: List[str] = []
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 logger = logging.getLogger("uvicorn")
+logger = logging.getLogger("gunicorn")
 logger.setLevel(logging.DEBUG) # the log level needs to be set here and not in uvicorn!
 
 
@@ -31,46 +54,86 @@ def next_batch(gen,n):
 
 # opening pickle file
 f = open("app/models/predictions.pickle","rb")
-df = pickle.load(f)
+df = pickle5.load(f)
 
 logger.debug(f'number of items in dataset {len(df)}')
 img_batch_gen = gen_img_ids()
 
 import app.database as db
 conn = db.open_db('app/predictions.db')
-db.print_db(conn)
+# db.print_db(conn)
+print('started')
 logger.debug(f'{db.count_predictions(conn)} items in database')
+import time
+from datetime import datetime
 
-@app.get("/get_images/") 
+
+
+@app.get("/backend/get_images/") 
 async def return_images():
-    logger.debug('get images') # this is the way to use the pydantic base model
-    n=10
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Current Time =", current_time)
+
+    t0 = time.time()
+    # logger.debug('get images') # this is the way to use the pydantic base model
+    # n=10
 
     batch_info_df = df.loc[next_batch(img_batch_gen,4)]
 
     faces = list(range(10)) # this was a placeholder for images, but can be approached locally
-    faceids = ['../'/f for f in batch_info_df['path']]
+    faceids = ['../../'+f for f in batch_info_df['path']]
     computer = list(batch_info_df['pred'])
     actual = list(batch_info_df['actual'])
+    print(time.time()-t0)
+    # print(faces, faceids, computer, actual)
+    # return {'hi':'33'}
     return {'faces': faces, 
             'faceids': faceids,
             'computer': computer,
             'actual': actual
             }
 
-@app.post("/submit_preds/") #use post since server receives
+@app.post("/backend/submit_preds/") #use post since server receives
 async def submit_preds(ages:Ages,request: Request):
     logger.debug('submit preds') # this is the way to use the pydantic base model
     logger.debug(request.client.host) # this is the way to use the pydantic base model
     logger.debug(ages)
-    # TODO save ages to database
-    for i in range(len(ages.age)):
-        db.create_pred(conn, [request.client.host,ages.faceids[i],ages.age[i]])
-    db.print_db(conn)
+    print(ages)
+    # save ages to database
+    if ages.age and ages.faceids:
+        for i in range(len(ages.age)):
+            db.create_pred(conn, [request.client.host,ages.faceids[i],ages.age[i]])
+            print('added')
+    # db.print_db(conn)
 
 
     return {'msg': 'ok'}
 
+
+
+
+@app.post("/backend/upload/")
+async def create_file(file: bytes = File(...)):
+    print('jo')
+    # 
+    try:
+        pil_image = np.array(Image.open(BytesIO(file)))
+
+    except:
+        return {"file_size": '2'}
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Unable to process file"
+        )
+    print(np.array(pil_image).shape)
+
+    return {"file_size": '4'}
+
+@app.post("/backend/test/")
+async def create_file():
+    print('jo')
+
+    return {"file_size": 'success'}
 #%%
 
 
